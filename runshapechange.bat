2bdd9f29b642dffca16aed5b9b32772589a86424
jetgeo <knut@jetlundweb.no>
2016-11-21 13:05:12 +0100

    Riktig versjon av NVDB-xsd-er

--- runshapechange.bat
+++ runshapechange.bat
@@ -1,1 +1,4 @@
-"java" -Dfile.encoding=UTF-8 -jar "C:\Program Files (x86)\Arkitektum AS\ShapeChange\ShapeChange.jar" -c "C:\DATA\GitHub\NVDBGML\config\ShapeChangeConfiguration.xml"
+"java" -Dfile.encoding=UTF-8 -Dhttp.proxyHost=proxy.vegvesen.no -Dhttp.proxyPort=8080 -jar "C:\Program Files (x86)\Arkitektum AS\ShapeChange\ShapeChange.jar" -c "C:\DATA\GitHub\NVDBGML\config\ShapeChangeConfiguration.xml"
+Mkdir C:\DATA\GitHub\NVDBGML\XSD\NVDB\107
+Del /Q C:\DATA\GitHub\NVDBGML\XSD\NVDB\107\*.*
+Move C:\DATA\GitHub\NVDBGML\XSD\INPUT\*.* C:\DATA\GitHub\NVDBGML\XSD\NVDB\107\
